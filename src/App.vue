4a8c0ef2908da79f2a991f1b9437d616bc1fa142
wang <xiaoWangHi@outlook.com>
2020-03-18 20:10:24 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <script>
 export default {
-  created () {}
-}
+	created() {}
+};
 </script>
 
 <style>
 .container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	/* align-items: center; */
+	/* justify-content: space-between; */
+	/* padding: 200rpx 0; */
+	box-sizing: border-box;
 }
 /* this rule will be remove */
 * {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
+	transition: width 2s;
+	-moz-transition: width 2s;
+	-webkit-transition: width 2s;
+	-o-transition: width 2s;
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,34 +1,75 @@
 <template>
-    <div class="container">
-        首页
-        <vant-search placeholder='搜索'></vant-search>
-    </div>
+	<div class="container">
+		<van-row class="vanRow">
+			<van-col span="6">
+				<div class="imgDiv">
+					<img src="../../../static/images/home/logo.png" alt>
+				</div>
+			</van-col>
+			<van-col span="18">
+				<van-dropdown-menu class="dropdownMenu">
+					<van-dropdown-item v-model="shopName" :options="shopOptions"/>
+				</van-dropdown-menu>
+			</van-col>
+		</van-row>
+	</div>
 </template>
 
 <script>
 import wx from "@/utils/wx-api";
 import utils from "@/utils/utils";
-import CONFIG from "@/config"
+import CONFIG from "@/config";
 import { mapState, mapMutations } from "vuex";
 import { TEST, MASTER_KEY } from "@/store/mutation-type";
 
 export default {
-    data() {
-        return {
-            
-        };
-    },
-    computed: {
-        ...mapState(["test", "master_key"])
-    },
-    onLoad() {
-        
-    },
-    methods: {
-        ...mapMutations([TEST, MASTER_KEY]),
-    }
+	data() {
+		return {
+			shopName: 0,
+			shopOptions: [
+				{ text: "全部商品", value: 0 },
+				{ text: "新款商品", value: 1 },
+				{ text: "活动商品", value: 2 }
+			]
+		};
+	},
+	computed: {
+		...mapState(["test", "master_key"])
+	},
+	onLoad() {},
+	methods: {
+		...mapMutations([TEST, MASTER_KEY])
+	}
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+	.vanRow {
+		height: 50rpx;
+		overflow: hidden;
+		.imgDiv {
+			display: flex;
+			justify-content: flex-end;
+			align-items: center;
+			height: 46rpx;
+			img {
+				width: 85rpx;
+				height: 43rpx;
+			}
+		}
+		.selectShop {
+			height: 50rpx;
+			line-height: 50rpx;
+			padding-left: 4rpx;
+		}
+		.dropdownMenu {
+			display: flex;
+			justify-content: flex-start;
+			height: 50rpx;
+			align-items: center;
+			// font-size: $--text-nm;
+		}
+	}
+}
 </style>
