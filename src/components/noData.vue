47784f24afb7d83326c5722994ad150ced4d01f6
LionMusician <zzdongcoder@gmail.com>
2020-03-28 22:11:54 +0800

    暂无数据

--- src/components/noData.vue
+++ src/components/noData.vue
@@ -1,13 +1,29 @@
 <template>
-    <div>
-        
+    <div class="main">
+        <i class="iconfont iconzanwushuju"></i>
+        <p>{{text}}</p>
     </div>
 </template>
 <script>
 export default {
-    
+    props: {
+        text: {
+            type: String,
+            default: "暂无数据"
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
-
+.main {
+    @include wh(100%, 80vh);
+    @include fc(center);
+    .iconzanwushuju {
+        @include sc(80rpx, $--color-gray-c);
+    }
+    p {
+        @include sc($--text-xl, $--color-gray-c);
+        @include hh(60rpx);
+    }
+}
 </style>
--- src/pages/book/addressList/index.vue
+++ src/pages/book/addressList/index.vue
@@ -1,40 +1,45 @@
 <template>
     <div class="main">
+        <ul v-if="addrList.length" class="addr-list">
+            <li></li>
+        </ul>
+        <no-data v-else></no-data>
         <div class="bottom">
-            <van-button
-                type="primary"
-                size="normal"
-                color="#98C145"
-                block
-                @click="newAddress"
-            >新增地址</van-button>
+            <van-button type="primary" size="normal" color="#98C145" block @click="newAddress">新增地址</van-button>
         </div>
     </div>
 </template>
 <script>
+import noData from "@components/noData.vue";
 import wx from "@/utils/wx-api";
 export default {
+    data() {
+        return {
+            addrList: []
+        }
+    },
+    components: { noData },
     onLoad() {
-        wx.setNavigationBarTitle('收货地址');
+        wx.setNavigationBarTitle("收货地址");
         this.getReceiveAddress();
     },
     methods: {
         newAddress() {
-            wx.navigateTo(`../editAddress/main`)
+            wx.navigateTo(`../editAddress/main`);
         },
         // 获取地址列表
         getReceiveAddress() {
-			let data = {};
-			this.$http.getReceiveAddress(data).then(res => {
-				console.log(res);
-			});
-
+            let data = {};
+            this.$http.getReceiveAddress(data).then(res => {
+                console.log(res);
+            });
         }
-    },
+    }
 };
 </script>
 <style lang="scss" scoped>
 .main {
+    min-height: 100vh;
     .bottom {
         width: 100%;
         position: fixed;
