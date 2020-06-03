8133303d956d9a829640880270ecb60f755a10d0
LionMusician <zzdongcoder@gmail.com>
2020-06-03 11:18:53 +0800

    修改

--- src/pages/book/index.vue
+++ src/pages/book/index.vue
@@ -285,8 +285,8 @@ export default {
 	.top {
 		.menus {
 			width: 750rpx;
-			overflow-x: scroll;
-			overflow-y: hidden;
+			// overflow-x: scroll;
+			// overflow-y: hidden;
 			/deep/ .van-dropdown-menu {
 				display: -webkit-flex;
 				display: -ms-flexbox;
